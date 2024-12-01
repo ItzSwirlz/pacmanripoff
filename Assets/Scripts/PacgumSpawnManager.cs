03d45fc7c2446205bc13533d51f17f54e7cf01b8
shreejalearn <shreeja.das.16@gmail.com>
2024-11-30 22:00:26 -0500

    Edited to make Pacgum appear randomly every couple of seconds rather than just once in gameplayloop

--- Assets/Scripts/PacgumSpawnManager.cs
+++ Assets/Scripts/PacgumSpawnManager.cs
@@ -21,35 +21,36 @@ public class PacgumSpawnController : MonoBehaviour
         BottomRightY = -3
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        Vector3 pos = new Vector3(0, 0, 0); // default needed for the compiler
-        
-        // Choose a random location
-        int randomInt = Random.Range(0, 4);
-        switch(randomInt) {
-            case 0:
-                pos = new Vector3((float) Location.TopLeftX, (float) Location.TopLeftY, 0);
-                break;
-            case 1:
-                pos = new Vector3((float) Location.TopRightX, (float) Location.TopRightY, 0);
-                break;
-            case 2:
-                pos = new Vector3((float) Location.BottomLeftX, (float) Location.BottomLeftY, 0);
-                break;
-            case 3:
-                pos = new Vector3((float) Location.BottomRightX, (float) Location.BottomRightY, 0);
-                break;
-        }
-
-        Instantiate(pacGum, pos, Quaternion.identity);
-
+        StartCoroutine(SpawnPacgum());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnPacgum()
     {
-        
+        while (true)
+        {
+            Vector3 pos = new Vector3(0, 0, 0);
+            
+            int randomInt = Random.Range(0, 4);
+            switch(randomInt) {
+                case 0:
+                    pos = new Vector3((float) Location.TopLeftX, (float) Location.TopLeftY, 0);
+                    break;
+                case 1:
+                    pos = new Vector3((float) Location.TopRightX, (float) Location.TopRightY, 0);
+                    break;
+                case 2:
+                    pos = new Vector3((float) Location.BottomLeftX, (float) Location.BottomLeftY, 0);
+                    break;
+                case 3:
+                    pos = new Vector3((float) Location.BottomRightX, (float) Location.BottomRightY, 0);
+                    break;
+            }
+
+            Instantiate(pacGum, pos, Quaternion.identity);
+
+            yield return new WaitForSeconds(3.0f);
+        }
     }
 }
